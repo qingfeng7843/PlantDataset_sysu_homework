73506ded1184a1c6d85d62f9b11f168806cfba16
qingfeng7843 <161007788+qingfeng7843@users.noreply.github.com>
2024-11-05 22:42:08 +0800

    Create infer.py

---
+++ infer.py
@@ -0,0 +1,42 @@
+import torchvision.models as models
+#!pip install albumentations==0.4.6
+#!pip install -U git+https://github.com/albu/albumentations > /dev/null
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from albumentations import Blur, MotionBlur, HorizontalFlip, VerticalFlip, GaussNoise, IAASharpen, RandomBrightnessContrast
+from torchvision import transforms as T
+import gc
+import torch 
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import os
+import pandas as pd
+
+from model_save_load import save_model, load_model
+from dataset import Plant_Dataset
+from train import train, train_val, validation
+from test import model_test
+gc.collect() 
+torch.cuda.empty_cache()
+
+test_folder_path = "/media/tiankanghui/plant_mymodel/plant_dataset/test/images"
+test_path = os.listdir(test_folder_path)
+test_csv_path = "/media/tiankanghui/plant_mymodel/plant_dataset/test/labels.csv"
+test_df = pd.read_csv(test_csv_path)
+test_df_cp = test_df.copy()
+
+test_transforms = A.Compose([A.Normalize(),ToTensorV2()])
+test_data = Plant_Dataset(test_folder_path,test_path,test_df_cp, transforms=test_transforms,train=False)
+test_loader = DataLoader(test_data, batch_size=1, shuffle=False,num_workers=0)
+
+
+model = models.swin_b(weights=None)
+model.head = nn.Linear(1024, 6)
+model.load_state_dict(torch.load("/media/tiankanghui/plant_mymodel/pretrained/swin_b-68c6b09e.pth"))
+
+images,predictions, acc = model_test(model,test_loader)
+
+print("Model Swin_b Test Acc:" ,acc)
+
+
